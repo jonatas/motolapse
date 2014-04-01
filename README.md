8bcb5715aab3ea37b069cd1d9604943670b6e8a6
Jonatas Davi Paganini <jonatasdp@gmail.com>
2014-04-01 01:01:55 -0300

    refactoring and creating readme

--- README.md
+++ README.md
@@ -2,3 +2,30 @@ motolapse
 =========
 
 Rotative timelapse experiments using raspberry pi, the official pi cam, a ULN2003 and a 28BYJ-48 motor
+
+## configure
+
+Search and exchange the 3 variables to work with your needs,
+
+### Configure the ULN2003 pins
+
+Change the pins on the following line to configure your own motors. The four respective values is ordered by 1N1,1N2,1N3,1N4.
+
+```python
+PINS_ON_ULN = [17,18,23,22]
+```
+### How much do you want to rotate?
+
+```python
+ROTATION_ANGLE = 180
+```
+### How much pictures?
+
+Configure the degrees per picture taken.
+
+```python
+PICTURE_EACH_HOW_MUCH_DEGREES = 5
+```
+
+
+
--- run.py
+++ run.py
@@ -1,20 +1,22 @@
-
 import RPi.GPIO as GPIO
 import picamera
+
 execfile("motor.py")
+
+GPIO.setmode(GPIO.BCM)
+
+PINS_ON_ULN = [17,18,23,22]
+ROTATION_ANGLE = 180
+PICTURE_EACH_HOW_MUCH_DEGREES = 5
+
+motor = Motor(PINS_ON_ULN)
+motor.rpm = 1
 with picamera.PiCamera() as camera:
-    GPIO.setmode(GPIO.BCM)
-    m = Motor([17,18,23,22])
-    m.rpm = 1
-    print "ok intialized"
     degree = 0
-    rotationAngle = 180
-    rotationStep = 5
-    camera.capture('deg0.jpg')
-    while degree < rotationAngle:
-	degree += rotationStep 
-	print "move to "+str(degree)
-        m.move_to(degree)
+    while degree < ROTATION_ANGLE:
         filename = 'deg'+str(degree)+'.jpg'
         print "Capturing: " + filename
         camera.capture(filename)
+	degree += PICTURE_EACH_HOW_MUCH_DEGREES 
+	print "move to "+str(degree)
+        motor.move_to(degree)
