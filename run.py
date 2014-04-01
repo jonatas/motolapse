d058f143beaa97e41a974bc5db63eaefaaee8472
Jonatas Davi Paganini <jonatasdp@gmail.com>
2014-04-01 14:30:45 -0300

    sleep each step

--- run.py
+++ run.py
@@ -1,17 +1,23 @@
 import RPi.GPIO as GPIO
 import picamera
+from time import sleep
 from motor import Motor
 
 GPIO.setmode(GPIO.BCM)
 
-PINS_ON_ULN = [17,18,23,22]
-ROTATION_ANGLE = 180
-PICTURE_EACH_HOW_MUCH_DEGREES = 5
+PINS_ON_ULN = [17,18,22,23]
+ROTATION_ANGLE = 225
+PICTURE_EACH_HOW_MUCH_DEGREES = 1
+MILLISECONDS_OF_WORK = 4 * 60 * 60 * 1000
+STEPS = ROTATION_ANGLE / PICTURE_EACH_HOW_MUCH_DEGREES
+SLEEP_PER_STEP = MILLISECONDS_OF_WORK / STEPS
+SLEEP_PER_PICTURE = 10000
 
+print "Sleep per step: " + str(SLEEP_PER_STEP)
 motor = Motor(PINS_ON_ULN)
 motor.rpm = 1
 with picamera.PiCamera() as camera:
-    degree = 0
+    degree = 0.0
     while degree < ROTATION_ANGLE:
         filename = 'deg'+str(degree)+'.jpg'
         print "Capturing: " + filename
@@ -19,3 +25,4 @@ with picamera.PiCamera() as camera:
 	degree += PICTURE_EACH_HOW_MUCH_DEGREES 
 	print "move to "+str(degree)
         motor.move_to(degree)
+	sleep(SLEEP_PER_STEP)
